302d868b43140e8f5e7b1ee6cb275d0bd1ed294b
linearediego <diegolineares@gmail.com>
2026-09-04 18:40:43 -0300

    feat: agregar slider page

--- app_components/lib/src/pages/slider_page.dart
+++ app_components/lib/src/pages/slider_page.dart
@@ -1,18 +1,80 @@
 import 'package:flutter/material.dart';
 
-class SliderPage extends StatelessWidget {
+class SliderPage extends StatefulWidget {
   const SliderPage({super.key});
+  @override
+  State<SliderPage> createState() => _SliderPageState();
+}
 
+class _SliderPageState extends State<SliderPage> {
+  double _valorSlider = 200.0;
+  bool _bloquearCheck = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Slider - Checks")),
-      body: Text("Slider - Checks"),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.pop(context);
-        },
+      appBar: AppBar(title: const Text('Slider')),
+      body: Container(
+        padding: const EdgeInsets.only(top: 50.0),
+        child: Column(
+          children: <Widget>[
+            _crearSlider(),
+            _crearCheckbox(),
+            _crearSwitch(),
+            Expanded(child: _crearImagen()),
+          ],
+        ),
       ),
+    );
+  }
+
+  Widget _crearSlider() {
+    return Slider(
+      activeColor: Colors.indigoAccent,
+      label: 'Tamaño de imagen',
+      value: _valorSlider,
+      min: 10.0,
+      max: 400.0,
+      onChanged: (_bloquearCheck)
+          ? null
+          : (valor) {
+              setState(() {
+                _valorSlider = valor;
+              });
+            },
+    );
+  }
+
+  Widget _crearCheckbox() {
+    return CheckboxListTile(
+      title: const Text('Checkbox bloqueo slider'),
+      value: _bloquearCheck,
+      onChanged: (valor) {
+        setState(() {
+          _bloquearCheck = valor!;
+        });
+      },
+    );
+  }
+
+  Widget _crearSwitch() {
+    return SwitchListTile(
+      title: const Text('Switch bloqueo slider'),
+      value: _bloquearCheck,
+      onChanged: (valor) {
+        setState(() {
+          _bloquearCheck = valor;
+        });
+      },
+    );
+  }
+
+  Widget _crearImagen() {
+    return Image(
+      image: const NetworkImage(
+        'https://kinsta.com/es/wp-content/uploads/sites/8/2021/12/back-end-developer.png',
+      ),
+      width: _valorSlider,
+      fit: BoxFit.contain,
     );
   }
 }
